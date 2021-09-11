d2096b52c545aa554a6bd61ebbca1fec140e7f1f
Junghoon Noh <jungnoh.dev@gmail.com>
2021-09-11 17:15:45 +0900

    Fix prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   'extends': [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier'
+    'plugin:prettier/recommended'
   ],
   'parser': '@typescript-eslint/parser',
   'parserOptions': {
